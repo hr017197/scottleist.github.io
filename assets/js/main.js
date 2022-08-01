fa978857f034177011c2b59790016303d1ab5dca
hr017197 <89270206+hr017197@users.noreply.github.com>
2022-07-31 20:07:38 -0600

    Possible outline

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,91 +5,91 @@
 */
 
 (function($) {
-/*js for playbutton*/
-TweenMax.set(".play-circle-01", {
-	rotation: 90,
-	transformOrigin: "center"
-  })
-  
-  TweenMax.set(".play-circle-02", {
-	rotation: -90,
-	transformOrigin: "center"
-  })
-  
-  TweenMax.set(".play-perspective", {
-	xPercent: 6.5,
-	scale: .175,
-	transformOrigin: "center",
-	perspective: 1
-  })
-  
-  TweenMax.set(".play-video", {
-	visibility: "hidden",
-	opacity: 0,
-  })
-  
-  TweenMax.set(".play-triangle", {
-	transformOrigin: "left center",
-	transformStyle: "preserve-3d",
-	rotationY: 10,
-	scaleX: 2
-  })
-  
-  const rotateTL = new TimelineMax({ paused: true })
-	.to(".play-circle-01", .7, {
-	  opacity: .1,
-	  rotation: '+=360',
-	  strokeDasharray: "456 456",
-	  ease: Power1.easeInOut
-	}, 0)
-	.to(".play-circle-02", .7, {
-	  opacity: .1,
-	  rotation: '-=360',
-	  strokeDasharray: "411 411",
-	  ease: Power1.easeInOut
-	}, 0)
-  
-  const openTL = new TimelineMax({ paused: true })
-	.to(".play-backdrop", 1, {
-	  opacity: .95,
-	  visibility: "visible",
-	  ease: Power2.easeInOut
-	}, 0)
-	.to(".play-close", 1, {
-	  opacity: 1,
-	  ease: Power2.easeInOut
-	}, 0)
-	.to(".play-perspective", 1, {
-	  xPercent: 0,
-	  scale: 1,
-	  ease: Power2.easeInOut
-	}, 0)
-	.to(".play-triangle", 1, {
-	  scaleX: 1,
-	  ease: ExpoScaleEase.config(2, 1, Power2.easeInOut)
-	}, 0)
-	.to(".play-triangle", 1, {
-	  rotationY: 0,
-	  ease: ExpoScaleEase.config(10, .01, Power2.easeInOut)
-	}, 0)
-	.to(".play-video", 1, {
-	  visibility: "visible",
-	  opacity: 1
-	}, .5)
-  
-  
-  const button = document.querySelector(".play-button")
-  const backdrop = document.querySelector(".play-backdrop")
-  const close = document.querySelector(".play-close")
-  
-  button.addEventListener("mouseover", () => rotateTL.play())
-  button.addEventListener("mouseleave", () => rotateTL.reverse())
-  button.addEventListener("click", () => openTL.play())
-  backdrop.addEventListener("click", () => openTL.reverse())
-  close.addEventListener("click", e => {
-	e.stopPropagation()
-	openTL.reverse()
-  })
+	/**
+	 * Get videos on load
+	 */
+	(function () {
+		getVideos();
+	})();
+	
+	/**
+	 * For each video player, create custom thumbnail or
+	 * use Youtube max resolution default thumbnail and create
+	 * iframe video.
+	 */
+	function getVideos() {
+		var v = document.getElementsByClassName("youtube-player");
+		for (var n = 0; n < v.length; n++) {
+			var p = document.createElement("div");
+			var id = v[n].getAttribute("data-id");
+	
+			var placeholder = v[n].hasAttribute("data-thumbnail")
+				? v[n].getAttribute("data-thumbnail")
+				: "";
+	
+			if (placeholder.length) p.innerHTML = createCustomThumbail(placeholder);
+			else p.innerHTML = createThumbail(id);
+	
+			v[n].appendChild(p);
+			p.addEventListener("click", function () {
+				var parent = this.parentNode;
+				createIframe(parent, parent.getAttribute("data-id"));
+			});
+		}
+	}
+	
+	/**
+	 * Create custom thumbnail from data-attribute provided url
+	 * @param {string} url
+	 * @return {string} The HTML containing the <img> tag
+	 */
+	function createCustomThumbail(url) {
+		return (
+			'<img class="youtube-thumbnail" src="' +
+			url +
+			'" alt="Youtube Preview" /><div class="youtube-play-btn"></div>'
+		);
+	}
+	
+	/**
+	 * Get Youtube default max resolution thumbnail
+	 * @param {string} id The Youtube video id
+	 * @return {string} The HTML containing the <img> tag
+	 */
+	function createThumbail(id) {
+		return (
+			'<img class="youtube-thumbnail" src="//i.ytimg.com/vi_webp/' +
+			id +
+			'/maxresdefault.webp" alt="Youtube Preview"><div class="youtube-play-btn"></div>'
+		);
+	}
+	
+	/**
+	 * Create and load iframe in Youtube container
+	 **/
+	function createIframe(v, id) {
+		var iframe = document.createElement("iframe");
+		console.log(v);
+		iframe.setAttribute(
+			"src",
+			"//www.youtube.com/embed/" +
+				id +
+				"?autoplay=1&color=white&autohide=2&modestbranding=1&border=0&wmode=opaque&enablejsapi=1&showinfo=0&rel=0"
+		);
+		iframe.setAttribute("frameborder", "0");
+		iframe.setAttribute("class", "youtube-iframe");
+		v.firstChild.replaceWith(iframe);
+	}
+	
+	/** Pause video on modal close **/
+	$("#video-modal").on("hidden.bs.modal", function (e) {
+		$(this).find("iframe").remove();
+	});
+	
+	/** Pause video on modal close **/
+	$("#video-modal").on("show.bs.modal", function (e) {
+		getVideos();
+	});
 
 	var	$window = $(window),
 		$body = $('body'),
